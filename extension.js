d6709749612418df18b762caad7eaae768ff6ff1
Denis Voskvitsov <denis.voskvitsov@gmail.com>
2018-05-01 23:37:04 +0300

    initial commit

---
+++ extension.js
@@ -0,0 +1,71 @@
+const St = imports.gi.St;
+const Meta = imports.gi.Meta;
+const Main = imports.ui.main;
+const Shell = imports.gi.Shell;
+const ExtensionUtils = imports.misc.extensionUtils;
+
+const Me = ExtensionUtils.getCurrentExtension();
+const Convenience = Me.imports.convenience;
+
+
+let _lastWsCbId, _lastWsId = [];
+let settings;
+
+
+function init() {
+    settings = Convenience.getSettings();
+}
+
+function _switchWorkspace() {
+    let _currentWorkspace = global.screen.get_active_workspace().index();
+    if (_lastWsId.length > 1) {
+        _lastWsId.pop(); // current ws
+        let lastWs = _lastWsId.pop();  // previous ws
+        _lastWsId.push(_currentWorkspace); // push current ws back
+        let metaWorkspace = global.screen.get_workspace_by_index(lastWs);  // switch to previous
+        metaWorkspace.activate(global.get_current_time());
+    }
+}
+
+function _findEmptyWorkspace() {
+    let metaWorkspace;
+
+    for (let i=0; i < global.screen.n_workspaces; i++) {
+        metaWorkspace = global.screen.get_workspace_by_index(i);
+        if (metaWorkspace.list_windows().length == 0) {
+            metaWorkspace.activate(global.get_current_time());
+            break;
+        }
+    }
+}
+
+function onWorkspaceSwitched() {
+    let _currentWorkspace = global.screen.get_active_workspace().index();
+    _lastWsId.push(_currentWorkspace);
+}
+
+function enable() {
+    _lastWsCbId = global.screen.connect('workspace-switched', onWorkspaceSwitched);
+
+    Main.wm.addKeybinding("switch-between-last-workspaces",
+        settings,
+        Meta.KeyBindingFlags.NONE,
+        Shell.ActionMode.NORMAL,
+        function(display, screen, window, binding) {
+            _switchWorkspace();
+        }
+    );
+
+    Main.wm.addKeybinding("switch-to-empty-workspace",
+        settings,
+        Meta.KeyBindingFlags.NONE,
+        Shell.ActionMode.NORMAL,
+        function(display, screen, window, binding) {
+            _findEmptyWorkspace();
+        }
+    );
+}
+
+function disable() {
+    global.display.disconnect(_lastWsCbId);
+}
